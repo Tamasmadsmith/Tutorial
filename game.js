ec3ac694a919cf46f1b8f5bb2ce67d333da8a8b6
Tamas <tamasmadsmith@gmail.com>
2026-06-23 09:38:01 +1200

    Add movement

--- game.js
+++ game.js
@@ -4,7 +4,37 @@ const ctx = canvas.getContext("2d");
 let x = 200;
 let y = 200;
 
+let dx = 20;
+let dy = 0;
+
+document.addEventListener("keydown", (event) => {
+    switch (event.key) {
+        case "ArrowUp":
+            dx = 0;
+            dy = -20;
+            break;
+
+        case "ArrowDown":
+            dx = 0;
+            dy = 20;
+            break;
+
+        case "ArrowLeft":
+            dx = -20;
+            dy = 0;
+            break;
+
+        case "ArrowRight":
+            dx = 20;
+            dy = 0;
+            break;
+    }
+});
+
 function draw() {
+    x += dx;
+    y += dy;
+
     ctx.clearRect(0, 0, 400, 400);
 
     ctx.fillStyle = "lime";
